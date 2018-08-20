3dad161d7c68c7a24052e134de63c57f0ee39875
aizquierdo <aizquierdo@mrmilu.com>
2018-08-20 11:05:39 +0200

    check if inventory path exists

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Alex Left',
     author_email='aizquierdo@mrmilu.com',
     url='https://github.com/mrmilu/ansible-tty',
-    version='0.1',
+    version='0.2',
     scripts=['ansible-tty'],
     license='GPL-v3',
     long_description=open('README.md').read(),
